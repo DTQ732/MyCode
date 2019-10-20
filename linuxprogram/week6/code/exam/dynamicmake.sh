8c5c7d5f6a0f583366f7f51301324e37aaf4d3fe
ddd <2370681859@qq.com>
2019-10-20 22:27:39 +0800

    add makeshell

--- linuxprogram/week6/code/exam/dynamicmake.sh
+++ linuxprogram/week6/code/exam/dynamicmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -v
 gcc -fPIC -shared -o ../dynamiclib/libmydld.so ../dynamiclib/mydld.c	#build dll command
-gcc -o slocaltest localtest.c -ldl -w		#compiler
+gcc -o tdylocaltest localtest.c -ldl -w		#compiler
 ########开始运行########
-./slocaltest					#run
+./tdylocaltest					#run
